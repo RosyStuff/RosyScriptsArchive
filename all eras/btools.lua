9057595692e976201e722931a2c697f895eac2f1
RosyStuff <rosykidd15@googlemail.com>
2026-03-16 02:20:15 +0600

    Create btools.lua

---
+++ all eras/btools.lua
@@ -0,0 +1,6 @@
+p = game.Players.LocalPlayer
+Instance.new("HopperBin", p.Backpack).BinType = 1
+Instance.new("HopperBin", p.Backpack).BinType = 2
+Instance.new("HopperBin", p.Backpack).BinType = 3
+Instance.new("HopperBin", p.Backpack).BinType = 4
+-- sup
