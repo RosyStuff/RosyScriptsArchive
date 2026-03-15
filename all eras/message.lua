99ae247104171acbea47a850d3185a6fe6ad0251
RosyStuff <rosykidd15@googlemail.com>
2026-03-16 02:14:54 +0600

    Create message.lua

---
+++ all eras/message.lua
@@ -0,0 +1,7 @@
+text = "wsp guys" -- change to your text you like
+duration = 3 -- change to your duration you like
+-- do not edit anything below
+msg = Instance.new('Message', workspace)
+msg = text
+wait(duration)
+msg:Destroy(-1) -- dont mind the -1 lol
